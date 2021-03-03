23b7527eaa1eddaffadc42016f906cfffc2e1379
iamkape <33788122+iamkape@users.noreply.github.com>
2021-03-03 22:11:34 +0300

    fixed : avg function

--- src/index.js
+++ src/index.js
@@ -9,5 +9,10 @@ exports.max = function max (array) {
 }
 
 exports.avg = function avg (array) {
+    
+    for(i=0; i<array.length; i++){
+      let sum+=array[i];
+    }
+    return sum/array.length;
   return 0;
 }
