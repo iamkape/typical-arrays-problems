cc1684c05acc667c677183d82bcf01d9c5523753
iamkape <33788122+iamkape@users.noreply.github.com>
2021-03-04 01:23:42 +0300

    fixed : min max

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 
 exports.min = function min (array) {
-   
+   if(array == undefined || array == 0){
+       return 0
+   } 
+    else 
+       {
   return Math.min.apply(null,array);
 }
-
+}
 exports.max = function max (array) {    
-    
+    if (array== 0 || array== undefined){
+        return 0
+    }
+    else {
   return Math.max.apply(null,array);
-}}
+}
+}
+
 exports.avg = function avg (array) {
     
     for(i=0; i<array.length; i++){
