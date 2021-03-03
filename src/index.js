a8c6dec36a5d09443b9737778f8731e838265f5d
iamkape <33788122+iamkape@users.noreply.github.com>
2021-03-04 00:09:30 +0300

    fixed : min max avg func

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 
 exports.min = function min (array) {
    
-  return Math.min.apply(null,min);
+  return Math.min.apply(null,array);
 }
 
 exports.max = function max (array) {    
     
-  return Math.max.apply(null,max);
+  return Math.max.apply(null,array);
 }}
 exports.avg = function avg (array) {
     
     for(i=0; i<array.length; i++){
-      let sum+=array[i];return sum/array.length;
+      let sum+=array[i];
+       
     }
-    
+     return sum/array.length;
   
 }
