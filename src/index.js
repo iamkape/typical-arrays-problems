d33d1d6651ee2fd143af57632750e9ae97cf08da
iamkape <33788122+iamkape@users.noreply.github.com>
2021-03-03 22:56:53 +0300

    fixed: max min func;

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
    
-  return Math.min.apply(null,array);
+  return Math.min.apply(null,min);
 }
 
 exports.max = function max (array) {    
     
-  return Math.max.apply(null,array);
+  return Math.max.apply(null,max);
 }}
 exports.avg = function avg (array) {
     
