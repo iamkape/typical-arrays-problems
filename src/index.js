7da2c6956a94fa9af07f1573973126e325b04c1e
iamkape <33788122+iamkape@users.noreply.github.com>
2021-03-03 22:31:12 +0300

    fixed : functions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 
 exports.min = function min (array) {
     
-  return Math.min(...array);
+  return Math.min.apply(...array);
 }
 
 exports.max = function max (array) {
-  return Math.max(...array);
+  return Math.max.apply(...array);
 }
 
 exports.avg = function avg (array) {
